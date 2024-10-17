02d5b1d0a4145357bfd7ebb0669addf3ea0eae97
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-10-17 22:26:55 +0530

    first commit

---
+++ frontend/my-project/src/components/HeroSection.jsx
@@ -0,0 +1,46 @@
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-3 lg:mt-5">
+      <span className="text-4xl sm:text-5xl lg:text-8xl text-center tracking-wide mb-3 font-serif bg-gradient-to-r from-indigo-600 to-violet-400 text-transparent bg-clip-text ">
+        SPARK
+      </span>
+      <h2 className="text-4xl sm:text-4xl lg:text-6xl text-center bg-gradien tracking-wide p-4 ">
+        Let's Collaborate, Connect & Grow
+        <span className="bg-gradient-to-r from-orange-600 to-violet-700 text-transparent bg-clip-text">
+          {" "}
+          for Students & Profesionalists
+        </span>
+      </h2>
+      <p className="mt-10 text-4xl text-center text-neutral-500 max-w-4xl">
+        Empower your creativity and bring your ideas to one of the most trusted
+        platform
+      </p>
+
+      <div className="flex mt-10 justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-12 my-4"
+        >
+          <source src={video1} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
+        >
+          <source src={video2} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ frontend/my-project/src/pages/Chatgpt/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { PiOpenAiLogoBold } from "react-icons/pi";
+
+const page = () => {
+  return (
+    <>
+      <div className="App">
+        <div className="sidebar">
+          <div className="upperSide">
+            <img src="" alt="" className="logo" />
+            <span className="">Apka-Bot</span>
+            <button className="btn">New Chat</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
